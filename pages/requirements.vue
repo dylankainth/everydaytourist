0494f74119ff7ffe814c0128827c141b9b266644
Dylan K <50585244+dylankainth@users.noreply.github.com>
2025-02-06 23:30:42 +0000

    started work on results page

--- pages/requirements.vue
+++ pages/requirements.vue
@@ -4,9 +4,15 @@
         <div class="container mx-auto px-5 pt-3 pb-5">
 
             <p class="text-3xl">Welcome</p>
-            <p class="text-lg text-gray-500">Let's find you some deals.</p>
+            <p class="text-lg text-gray-500">Let's find you something to do.</p>
 
 
+            <div class="mt-10">
+                <NuxtLink to="/results" type="button"
+                    class="text-white bg-columbia-600 hover:bg-columbia-700 focus:ring-4 focus:ring-columbia-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-columbia-600 dark:hover:bg-columbia-700 focus:outline-none dark:focus:ring-columbia-800">
+                    Next</NuxtLink>
+
+            </div>
         </div>
 
     </div>
---
+++ pages/results.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <div class="container mx-auto px-5 pt-3 pb-5">
+
+        <p class="text-3xl">Your results</p>
+        <p class="text-lg text-gray-500">We found this for you.</p>
+
+        <div class="flex justify-center pt-2">
+            <div class="inline-flex rounded-lg shadow-sm" role="group">
+                <button type="button"
+                    class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-500 focus:text-blue-700 flex items-center">
+                    <svg class="w-6 h-6 text-gray-800 dark:text-white mr-2" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
+                            d="M9 8h10M9 12h10M9 16h10M4.99 8H5m-.02 4h.01m0 4H5" />
+                    </svg>
+                    List View
+                </button>
+
+                <button type="button"
+                    class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-500 focus:text-blue-700 flex items-center">
+                    <svg class="w-6 h-6 text-gray-800 dark:text-white mr-2" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M12 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M17.8 13.938h-.011a7 7 0 1 0-11.464.144h-.016l.14.171c.1.127.2.251.3.371L12 21l5.13-6.248c.194-.209.374-.429.54-.659l.13-.155Z" />
+                    </svg>
+                    Map View
+                </button>
+            </div>
+        </div>
+
+    </div>
+</template>
