896f889eca1411154f7bafd8fa9b69e2ed5ddda2
Dylan K <50585244+dylankainth@users.noreply.github.com>
2025-02-07 12:15:29 +0000

    reformed login page

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -8,14 +8,29 @@ const { signIn, status, data } = useAuth()
 
 <template>
     <div>
-        <p>Sign-In Options:</p>
-        <button @click="signIn('google')">
-            Google
-        </button>
 
-        {{ status }}
+        <div class="flex min-h-screen items-center justify-center">
+            <div class="container mx-auto px-5 pt-3 pb-5">
+                <div class="flex justify-center">
+                    <div class="flex bg-gray-100 p-4 rounded-2xl shadow-md w-80">
+                        <div class="w-full text-center">
+                            <p class="font-bold text-lg text-gray-900">Log in</p>
 
-        {{ data }}
+                            <!-- add a signin with google button -->
+                            <div class="pt-5">
+                                <img src="/signinbutton.svg" alt="Sign in with Google" class=" h-70 mx-auto"
+                                    @click="signIn('google')" />
+                            </div>
+                        </div>
+
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
 
     </div>
 </template>
